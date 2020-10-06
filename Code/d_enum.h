93123cac0846dc91ea89d38d31b866709afbdb30
DavidEilenstein <59915628+DavidEilenstein@users.noreply.github.com>
2020-10-06 17:37:21 +0200

    Release
    Added the first release version.
    Doc is uncomplete, but the functionality of ImgeD is at a fine state.

--- Code/d_enum.h
+++ Code/d_enum.h
@@ -15,8 +15,8 @@
 #include <QString>
 #include <complex>
 
-const QString           D_QS_State    = "pre release";
-const QString           D_QS_Version  = "v0_1_1";
+const QString           D_QS_State    = "release";
+const QString           D_QS_Version  = "v1";
 const QString           D_QS_Release  = "06.10.2020";
 const QString           D_QS_Author   = "David Eilenstein";
 const QString           D_QS_Facility = "GSI Darmstadt";
